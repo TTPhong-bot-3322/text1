7cb3d1bf1d630b2743982b4d4f767be3992231a0
thong <thonghpc9825@gmail.com>
2024-11-28 10:09:40 +0700

    thong clinet hien thi sp

--- Models/sanpham.php
+++ Models/sanpham.php
@@ -37,15 +37,8 @@ function editSanPham($id, $name, $idDanhMuc, $gia, $mota, $fileName)
 //     $sql = "update danh_muc set status = '$status' where id_danh_muc='$id'";
 //     pdo_execute($sql);
 // }
-function getSanPhamGanDay() {
+function getsanphamganday() {
     $sql = 'select * from san_pham order by create_at DESC limit 3';
     return pdo_query($sql);
 }
 
-function getSanPhamReview() {
-    $sql = 'SELECT s.id_sp as id, s.hinhanh, s.ten_sp, s.gia_sp, s.loai, s.id_danh_muc, 
-    COUNT(b.noi_dung) as tongbinhluan FROM san_pham s INNER JOIN binh_luan b on 
-    s.id_sp = b.id_sp GROUP BY id, hinhanh, ten_sp, gia_sp, loai, id_danh_muc 
-    ORDER BY tongbinhluan DESC LIMIT 3';
-    return pdo_query($sql);
-}
